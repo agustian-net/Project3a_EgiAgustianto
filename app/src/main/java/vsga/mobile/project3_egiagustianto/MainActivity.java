c2b4aa90a1c3340926d91f5a8c69583693d510f9
egi-agustian <egiagustianto1994@gmail.com>
2021-08-05 23:55:47 +0700

    menambahkan menu Listview

--- app/src/main/java/vsga/mobile/project3_egiagustianto/MainActivity.java
+++ app/src/main/java/vsga/mobile/project3_egiagustianto/MainActivity.java
@@ -1,12 +1,12 @@
 package vsga.mobile.project3_egiagustianto;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ListView*;
+import android.widget.Toast*;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
