8661e525fe315efd80400916fcc8930c67b59867
egi-agustian <egiagustianto1994@gmail.com>
2021-08-10 21:03:14 +0700

    Menyelesaikan aplikasi

--- app/src/main/java/vsga/mobile/project3_egiagustianto/MainActivity.java
+++ app/src/main/java/vsga/mobile/project3_egiagustianto/MainActivity.java
@@ -1,14 +1,33 @@
 package vsga.mobile.project3_egiagustianto;
 
 import android.os.Bundle;
-import android.widget.ListView*;
-import android.widget.Toast*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
-      @Override
+    private final String[] namanegara = new String[]{
+            "Indonesia", "Malaysia", "Singapore",
+            "Italia", "Inggris", "Belanda",
+            "Argentina", "Chile", "Mesir", "Uganda"
+    };
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //menampilkan judul
+        getSupportActionBar().setTitle("ListView Sederhana");
+
+        //memformat data
+        ListView lvItem = (ListView) findViewById(R.id.listNegara);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namanegara);
+
+        //menset data didalam list view
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener((parent, view, position, id) ->
+                Toast.makeText(MainActivity.this, "Memilih : "+namanegara[position], Toast.LENGTH_LONG).show());
     }
 }
